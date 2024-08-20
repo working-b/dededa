0b05e545c1e70831a0e354ee97a13ae9af8fb497
han.zt <han.zt@asiainfo-sec.com>
2024-08-20 22:25:15 +0800

    - 初始化模版

---
+++ dededa-backend/src/main/java/com/guoshi/dededa/controller/PostThumbController.java
@@ -0,0 +1,56 @@
+package com.guoshi.dededa.controller;
+
+import com.guoshi.dededa.common.BaseResponse;
+import com.guoshi.dededa.common.ErrorCode;
+import com.guoshi.dededa.common.ResultUtils;
+import com.guoshi.dededa.model.dto.postthumb.PostThumbAddRequest;
+import com.guoshi.dededa.model.entity.User;
+import com.guoshi.dededa.exception.BusinessException;
+import com.guoshi.dededa.service.PostThumbService;
+import com.guoshi.dededa.service.UserService;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 帖子点赞接口
+ *
+ * @author <a href="https://github.com/liguoshi">程序员鱼皮</a>
+ * @from <a href="https://guoshi.icu">编程导航知识星球</a>
+ */
+@RestController
+@RequestMapping("/post_thumb")
+@Slf4j
+public class PostThumbController {
+
+    @Resource
+    private PostThumbService postThumbService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 点赞 / 取消点赞
+     *
+     * @param postThumbAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doThumb(@RequestBody PostThumbAddRequest postThumbAddRequest,
+                                         HttpServletRequest request) {
+        if (postThumbAddRequest == null || postThumbAddRequest.getPostId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能点赞
+        final User loginUser = userService.getLoginUser(request);
+        long postId = postThumbAddRequest.getPostId();
+        int result = postThumbService.doPostThumb(postId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
